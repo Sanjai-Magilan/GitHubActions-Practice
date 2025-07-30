7568694ab8af06ae04b1bc298188fe46a9bdec2a
Sanjai-Magilan <sanjai6115@gmail.com>
2025-07-30 13:44:08 +0530

    added new function and test

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ function sum(a, b) {
 }
 
 module.exports = sum;
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { sum, multiply };
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,3 +3,9 @@ const sum = require('../src/index');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const multi = require('../src/index');
+
+test('multip 2 * 3 equals 6',() => {
+  expect(multi.multiply(2, 3)).toBe(6);
+});
